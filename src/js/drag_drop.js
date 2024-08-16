5961dcb8d14b35d69489536e964ef46a062ce364
MVeerasi <mark.veerasingam@analog.com>
2024-08-16 18:37:49 +0100

    added drag and drop

--- src/js/drag_drop.js
+++ src/js/drag_drop.js
@@ -1,1 +1,47 @@
-const draggables = document.querySelectorAll('.draggable')
+const draggables = document.querySelectorAll('.draggable');
+const boxes = document.querySelectorAll('.box');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+    draggable.addEventListener('dragend', dragEnd);
+});
+
+boxes.forEach(box => {
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragenter', dragEnter);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+});
+
+function dragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    setTimeout(() => {
+        event.target.style.display = 'none';
+    }, 0);
+}
+
+function dragEnd(event) {
+    event.target.style.display = 'block';
+}
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function dragEnter(event) {
+    event.preventDefault();
+    event.target.classList.add('over');
+}
+
+function dragLeave(event) {
+    event.target.classList.remove('over');
+}
+
+function drop(event) {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    const draggable = document.getElementById(id);
+    event.target.classList.remove('over');
+    event.target.appendChild(draggable);
+    draggable.style.display = 'block';
+}
